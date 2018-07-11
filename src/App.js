29b670a4f986a7208a0ce331df210b7d85aac262
DESKTOP-2VO0926\carla <carlapapalardo@gmail.com>
2018-07-10 21:04:21 -0300

    understanding concepts of typescript and react

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26},
+    ]    
+  }
+
+  //handler indicates this is a 
+  //method your are not activelly calling
+  //but you are assigning as an event handler
+  switchNameHandler = (newName) => {
+    //console.log('Was clicked');
+    //DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons:[
+        {name: newName, age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27},
+    ]})
+  }
+
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons:[
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 29},
+        {name: 'Stephanie', age: 26},
+    ]})
+  }
+
+  //don't add parentheses to function on click
+  //button onClick example can be ineficcient. Use bind whenever possible
+  render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font:'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    return (
+      <div className="App">
+        <h1>{"Hi! I'm a react app!"}</h1>
+        <p>This is really working!</p>        
+        <button style={style}
+          onClick={() => this.switchNameHandler('Carla')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}>My hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
+      </div>      
+    );
+  }
+}
+
+export default App;
