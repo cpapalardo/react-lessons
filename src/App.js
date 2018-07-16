4f93a242d43bb2f9c81959881912a0c5a169d25a
DESKTOP-2VO0926\carla <carlapapalardo@gmail.com>
2018-07-15 21:48:56 -0300

    using Radium for scoped pseudo selectors and media queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -68,11 +69,16 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font:'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -93,24 +99,39 @@ class App extends Component {
             click={() => this.deletePersonHandler(index)}
             changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
-
-
-      </div> 
+      </div>       
       );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+    }
+
+    const classes = []; //= ['red', 'bold'].join(' ');
+
+    if(this.state.persons.length <= 2){
+      classes.push('red');      
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-        <h1>{"Hi! I'm a react app!"}</h1>
-        <p>This is really working!</p>        
-        <button style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+      <StyleRoot>
+        <div className="App">
+          <h1>{"Hi! I'm a react app!"}</h1>
+          <p className={classes.join(' ')}>This is really working!</p>        
+          <button style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
 
-          {persons}
+            {persons}
 
-      </div>      
+        </div>     
+      </StyleRoot> 
     );
   }
 }
-
-export default App;
+//higher order component
+export default Radium(App);
