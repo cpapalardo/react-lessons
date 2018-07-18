088f5f7744f693e64c4da1b6bc3a5fd87c642646
DESKTOP-2VO0926\carla <carlapapalardo@gmail.com>
2018-07-17 21:36:48 -0300

    improvements to the project structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from  './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
 
@@ -77,13 +76,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index)  => {
-            return <ErrorBoundary key={person.id}>
-              <Person 
+            return <Person 
               name={person.name} 
               age={person.age}
               click={() => this.deletePersonHandler(index)}
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>
-            </ErrorBoundary>
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+              key={person.id}/>            
           })}
       </div>       
       );
